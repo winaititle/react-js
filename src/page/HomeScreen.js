34ddf91e33959b8ce0ce1869005d7a2fc3cbea62
winai <winai.sr@rmuti.ac.th>
2023-08-05 02:58:13 +0700

    add botton edit upload profile

--- src/page/HomeScreen.js
+++ src/page/HomeScreen.js
@@ -1,17 +1,68 @@
-// rafce
 import { AppBar } from '@mui/material';
+import React, { useState } from 'react';
 import Navbar from "../components/Navbar";
+function AvatarUploader({ onUpdate }) {
+  const [selectedImage, setSelectedImage] = useState(null);
 
-function HomeScreen () {
-  return(
-    <AppBar>
-      <Navbar>
-      <h1>ssss</h1>
-      <h2>sssss</h2>
-      </Navbar>
-    </AppBar>
-   
-  );    
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedImage(URL.createObjectURL(file));
+  };
+
+  const handleUpload = () => {
+    // Simulate API call and updating user profile
+    // Replace this with your actual API integration
+    setTimeout(() => {
+      onUpdate(selectedImage);
+    }, 10);
+  };
+  const person = {
+    name: 'GOJO',
+    theme: {
+
+      color: 'black'
+    }
+  };
+  return (
+    <>
+      <AppBar>
+        <Navbar>
+        </Navbar>
+      </AppBar>
+      <div>
+      <h1>{person.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src={"https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/06/gojo2.jpg"} alt="Selected Avatar"
+        width={90}
+        >
+        </img>
+        <input type="file" accept="image/*" onChange={handleImageChange} />
+        {selectedImage && <button onClick={handleUpload}>Upload</button>}
+        <ul>
+        <li>test@gmail.com</li>
+        <li>6133211*****</li>
+        <li>090******</li>
+      </ul>
+      </div>
+    </>
+  );
 }
 
-export default HomeScreen
+function ProfilePage() {
+  const [avatar, setAvatar] = useState(null);
+
+  const handleAvatarUpdate = (newAvatar) => {
+    setAvatar(newAvatar);
+  };
+
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      {avatar && <img src={avatar}width={250} height={350}alt="Profile Avatar" />}
+      <AvatarUploader onUpdate={handleAvatarUpdate} />
+    </div>
+  );
+}
+
+export default ProfilePage;
--- src/page/Profile.js
+++ src/page/Profile.js
@@ -1,6 +1,13 @@
 import { AppBar } from "@mui/material";
 import Navbar from "../components/Navbar";
 
+function MyButton() {
+  return (
+    <button>
+      Edit
+    </button>
+  );
+}
 const person = {
   name: 'name',
   theme: {
@@ -17,6 +24,7 @@ export default function TodoList() {
       
     </Navbar>
     </AppBar>
+    
     <div style={person.theme}>
       <h1>{person.name}'s Todos</h1>
       <img
@@ -24,14 +32,16 @@ export default function TodoList() {
         src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/06/gojo2.jpg"
         alt="GOJO"
         width={500}
+        
       />
-      
+      <MyButton/>
       <ul>
         <li>test@gmail.com</li>
         <li>6133211*****</li>
         <li>090******</li>
       </ul>
     </div>
+    
     </>
   );
 }
