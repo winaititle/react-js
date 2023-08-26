780450f8b12184671bde914dd770899581710f41
winai <winai.sr@rmuti.ac.th>
2023-08-26 23:58:44 +0700

    edit page home

--- src/page/Home.js
+++ src/page/Home.js
@@ -9,74 +9,54 @@ const CourseList = () => {
     {
       id: 1,
       title: "บทที่ 1",
-      description: "Learn the basics of programming.",
+      description: "ความรู้เบื้องต้นเกี่ยวกับคอมพิวเตอร์",
       imageURL:
-        "https://th.bing.com/th/id/OIP.7dbrfAdTIKG3qYKg-b2mCgHaEc?pid=ImgDet&rs=1",
+        "https://www.britishcouncil.or.th/sites/default/files/styles/bc-landscape-630x354/public/10-sentences-and-phrases-to-begin-business-email.png?itok=VEctoU3W",
       with: 150,
     },
     {
       id: 2,
       title: "บทที่ 2",
-      description: "Build modern web applications.",
+      description: "องค์ประกอบของระบบคอมพิวเตอร์",
       imageURL:
-        "https://www.aaup.edu/sites/default/files/What-is-eLearning.jpg",
+        "https://www.fau.edu/oit/services/images/fixing.jpg",
     },
     {
       id: 3,
       title: "บทที่ 3",
-      description: "Build modern web applications.",
+      description: "คอมพิวเตอร์ กับ ระบบเลขฐาน",
       imageURL:
-        "https://www.99techpost.com/wp-content/uploads/2018/10/e-Learning-1024x723.jpg",
+        "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG1hdGh8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
     },
     {
       id: 4,
       title: "บทที่ 4",
-      description: "Build modern web applications.",
+      description: "ฮาร์ดแวร์และอุปกรณ์ที่เกี่ยวข้อง",
 
       imageURL:
-        "https://th.bing.com/th/id/OIP.EMA7vc_jex5R-1xQARkJYQHaEK?pid=ImgDet&rs=1",
+        "https://images.unsplash.com/photo-1562408590-e32931084e23?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
     },
     {
       id: 5,
       title: "บทที่ 5",
-      description: "Build modern web applications.",
-      imageURL: "https://noobie.com/wp-content/uploads/2021/08/e-learning.jpg",
+      description: "ซอฟต์แวร์และภาษาคอมพิวเตอร์",
+      imageURL: "https://media.istockphoto.com/id/1445262527/th/%E0%B8%A3%E0%B8%B9%E0%B8%9B%E0%B8%96%E0%B9%88%E0%B8%B2%E0%B8%A2/%E0%B8%99%E0%B8%B1%E0%B8%81%E0%B8%9E%E0%B8%B1%E0%B8%92%E0%B8%99%E0%B8%B2%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%80%E0%B8%82%E0%B9%89%E0%B8%B2%E0%B8%A3%E0%B8%AB%E0%B8%B1%E0%B8%AA%E0%B9%83%E0%B8%99%E0%B9%81%E0%B8%A5%E0%B9%87%E0%B8%9B%E0%B8%97%E0%B9%87%E0%B8%AD%E0%B8%9B-%E0%B9%81%E0%B8%99%E0%B8%A7%E0%B8%84%E0%B8%B4%E0%B8%94%E0%B8%8B%E0%B8%AD%E0%B8%9F%E0%B8%95%E0%B9%8C%E0%B9%81%E0%B8%A7%E0%B8%A3%E0%B9%8C%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%80%E0%B8%82%E0%B8%B5%E0%B8%A2%E0%B8%99%E0%B9%82%E0%B8%9B%E0%B8%A3%E0%B9%81%E0%B8%81%E0%B8%A3%E0%B8%A1.jpg?s=612x612&w=0&k=20&c=hZgh0BWZVqhxb3YtfcA4YYrsap2_sPElSXeBejkWpPI=",
     },
     {
       id: 6,
       title: "บทที่ 6",
-      description: "Build modern web applications.",
+      description: "ระบบเครือข่ายคอมพิวเตอร์",
       imageURL:
-        "https://i0.wp.com/boletines.guanajuato.gob.mx/wp-content/uploads/2020/03/iStock-985024622.jpg?w=3000&ssl=1",
+        "https://images.unsplash.com/photo-1558494949-ef010cbdcc31?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1934&q=80",
     },
     {
       id: 7,
       title: "บทที่ 7",
-      description: "Build modern web applications.",
+      description: "อินเทอร์เน็ตและบริการออนไลน์",
       imageURL:
-        "https://i0.wp.com/boletines.guanajuato.gob.mx/wp-content/uploads/2020/03/iStock-985024622.jpg?w=3000&ssl=1",
+        "https://media.istockphoto.com/id/1414916304/th/%E0%B8%A3%E0%B8%B9%E0%B8%9B%E0%B8%96%E0%B9%88%E0%B8%B2%E0%B8%A2/%E0%B9%81%E0%B8%99%E0%B8%A7%E0%B8%84%E0%B8%B4%E0%B8%94%E0%B9%80%E0%B8%9B%E0%B9%89%E0%B8%B2%E0%B8%AB%E0%B8%A1%E0%B8%B2%E0%B8%A2%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%9E%E0%B8%B1%E0%B8%92%E0%B8%99%E0%B8%B2%E0%B8%97%E0%B8%B5%E0%B9%88%E0%B8%A2%E0%B8%B1%E0%B9%88%E0%B8%87%E0%B8%A2%E0%B8%B7%E0%B8%99-%E0%B8%88%E0%B8%B1%E0%B8%9A%E0%B8%A1%E0%B8%B7%E0%B8%AD%E0%B9%80%E0%B8%84%E0%B8%A3%E0%B8%B7%E0%B8%AD%E0%B8%82%E0%B9%88%E0%B8%B2%E0%B8%A2%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%AA%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%AA%E0%B8%B2%E0%B8%A3%E0%B8%97%E0%B8%B1%E0%B9%88%E0%B8%A7%E0%B9%82%E0%B8%A5%E0%B8%81%E0%B8%97%E0%B8%B5%E0%B9%88%E0%B8%A1%E0%B8%B5%E0%B9%84%E0%B8%AD%E0%B8%84%E0%B8%AD%E0%B8%99%E0%B8%AA%E0%B8%A0%E0%B8%B2%E0%B8%9E%E0%B9%81%E0%B8%A7%E0%B8%94%E0%B8%A5.jpg?s=612x612&w=0&k=20&c=H4i1TQ8kzP051Il05ci8leOhStF_GaKzYNVQDcmicyw=",
     },
-    {
-      id: 8,
-      title: "บทที่ 8",
-      description: "Build modern web applications.",
-      imageURL:
-        "https://i0.wp.com/boletines.guanajuato.gob.mx/wp-content/uploads/2020/03/iStock-985024622.jpg?w=3000&ssl=1",
-    },
-    {
-      id: 9,
-      title: "บทที่ 9",
-      description: "Build modern web applications.",
-      imageURL:
-        "https://i0.wp.com/boletines.guanajuato.gob.mx/wp-content/uploads/2020/03/iStock-985024622.jpg?w=3000&ssl=1",
-    },
-    {
-      id: 10,
-      title: "บทที่ 10",
-      description: "Build modern web applications.",
-      imageURL:
-        "https://i0.wp.com/boletines.guanajuato.gob.mx/wp-content/uploads/2020/03/iStock-985024622.jpg?w=3000&ssl=1",
-    },
+    
     // เพิ่มคอร์สเรียนเพิ่มเติมตามต้องการ
   ];
 
